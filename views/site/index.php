27a5d80539e3eef79faa52fe1c76512dec6a5c12
guillez <ingguillemroz@gmail.com>
2016-03-28 23:48:10 -0300

    principal

--- views/site/index.php
+++ views/site/index.php
@@ -6,19 +6,12 @@ $this->title = 'Sistema de Proveedores';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Bienvenido a S.I.G.A.</h1>
-
-        <p class="lead">Sistema Integral de Gestión Académica</p>
-
-       
-    </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-             <img data-src="gestion.png" class="img_1" src="gestion.png" alt="Image 1"/>
+             <img data-src="directivo.jpg" class="img_1" src="directivo.jpg" alt="Image 1"/>
                 <h2>Gestión</h2>
 
                 <p>Mediante las configuraciones generales del sistema SIGA, se podra adaptar las características particulaes de las escuelas a la funcionalida deseada.</p>
